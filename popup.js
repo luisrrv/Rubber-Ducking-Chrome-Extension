09faa0bffb081bd0efd2a53f57a00c71bae194e0
luis rodriguez <rodluisr@gmail.com>
2022-07-14 16:46:46 +0900

    basic rubberducking

--- popup.js
+++ popup.js
@@ -1,6 +1,31 @@
-const addNewBookmark = () => {};
+const duckDiv = document.querySelector(".duck");
+const introduction = document.querySelector(".introduction");
 
-const viewBookmarks = () => {};
+const showDuckImg = () => {
+  duckDiv.style.backgroundImage = `url(./assets/${Math.floor(Math.random() * 6) + 1}.png)`
+};
+
+const duckName = () => {
+   if (duckDiv.style.backgroundImage === `url("./assets/1.png")`) {
+    return "The OG Rubber Duck";
+  } else if (duckDiv.style.backgroundImage === `url("./assets/2.png")`) {
+    return "Green Rubber Duck"
+  } else if (duckDiv.style.backgroundImage === `url("./assets/3.png")`) {
+    return " Cool Rubber Duck"
+  } else if (duckDiv.style.backgroundImage === `url("./assets/4.png")`) {
+    return "Breaking Bath Rubber Duck"
+  } else if (duckDiv.style.backgroundImage === `url("./assets/5.png")`) {
+    return "Joker Rubber Duck"
+  } else if (duckDiv.style.backgroundImage === `url("./assets/6.png")`) {
+    return "Ryu Rubber Duck"
+  } else {
+    return ""
+  }
+};
+
+const duckIntro = () => {
+  introduction.innerHTML = `<h3>${duckName()} is here to help!</h3>`
+};
 
 const onPlay = e => {};
 
@@ -8,4 +33,7 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  showDuckImg();
+  duckIntro();
+});
